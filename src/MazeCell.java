91ba14b7d54fcd7a312564302d8f494fd3f9aaf1
Olivia Kinnear <git@superatomic.dev>
2025-04-16 10:17:08 -0500

    Added MazeCell

---
+++ src/MazeCell.java
@@ -0,0 +1,19 @@
+public class MazeCell {
+    private boolean wall; // whether the tile is a wall
+
+    private MazeCell(boolean wall) {
+        this.wall = wall;
+    }
+
+    public static MazeCell createWall() {
+        return new MazeCell(true);
+    }
+
+    public static MazeCell createPath() {
+        return new MazeCell(false);
+    }
+
+    public boolean isWall() {
+        return wall;
+    }
+}
