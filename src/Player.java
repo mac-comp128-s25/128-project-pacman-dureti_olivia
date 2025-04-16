5079c45493bf540679b410b934deadb7631c88f1
Olivia Kinnear <git@superatomic.dev>
2025-04-16 10:08:25 -0500

    Scaled pacman

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,10 @@
 import edu.macalester.graphics.Image;
 
 public class Player extends Image {
+    public static final double SIZE = 48;
+
     public Player() {
         super("pacman.png");
+        setMaxHeight(SIZE);
     }
 }
