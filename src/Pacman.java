aadfc1657f7550d1a31bbc85cdc3233ac479eda2
Olivia Kinnear <git@superatomic.dev>
2025-04-16 09:51:35 -0500

    Added Pacman class

---
+++ src/Pacman.java
@@ -0,0 +1,11 @@
+import edu.macalester.graphics.CanvasWindow;
+
+public class Pacman extends CanvasWindow {
+    public Pacman() {
+        super("Pacman", 1000, 1000);
+    }
+
+    public static void main(String[] args) {
+        new Pacman();
+    }
+}
