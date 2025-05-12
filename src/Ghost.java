b6ab2ee3a1a7c422378fc5384c3e375846c926eb
Olivia Kinnear <git@superatomic.dev>
2025-05-11 19:19:46 -0500

    Fix merge conflict

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,10 +1,36 @@
 import edu.macalester.graphics.Image;
+import edu.macalester.graphics.Point;
 
-public class Ghost extends Image {
+public abstract class Ghost extends Image {
     public static final double SIZE = 25;
 
-    public Ghost() {
+    private int x, y;
+    private int heading = Direction.NONE;
+
+    public Ghost(int x, int y) {
         super("inky.png");
         setMaxHeight(SIZE);
+        this.x = x;
+        this.y = y;
+        syncPosition();
+    }
+
+    public void syncPosition() {
+        setCenter(x*MazeCell.SIZE+(MazeCell.SIZE/2), y*MazeCell.SIZE+(MazeCell.SIZE/2));
+    }
+
+    public int cellX() {
+        return x;
+    }
+
+    public int cellY() {
+        return y;
+    }
+
+    private int nextDirection() {
+    }
+
+    private abstract Point target() {
+
     }
 }
